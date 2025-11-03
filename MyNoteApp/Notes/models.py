26295c424bc31a371bf7d7191504e4955b8458dc
Ayo45-alt <ayologr8@gmail.com>
2025-11-03 02:40:38 +0100

    a message here

--- MyNoteApp/Notes/models.py
+++ MyNoteApp/Notes/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Models):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.username
+
+
+class Notes(models.Models):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    images = models.ImageField(upload_to='', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+class Tasks(models.Models):
+    task_name = models.CharField(max_length=200)
+    is_completed = models.BooleanField(default=False)
+    due_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.task_name
+    
+
+
